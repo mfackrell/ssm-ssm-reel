df731880a04a7027ba6c7ffa0b5387b8b632d183
mfackrell <mfackrell@contract-cfo.com>
2026-02-01 13:38:41 -0600

    Update triggerZapier.js

--- steps/triggerZapier.js
+++ steps/triggerZapier.js
@@ -4,7 +4,7 @@ export async function triggerZapier(payload) {
   console.log("Triggering Zapier Webhook...");
   console.log("Payload being sent:", JSON.stringify(payload, null, 2)); // Log payload for debugging
 
-  const webhookUrl = "https://hooks.zapier.com/hooks/catch/19867794/uw0y7qi/";
+  const webhookUrl = "https://hooks.zapier.com/hooks/catch/19867794/uligcc7/";
 
   try {
     const response = await fetch(webhookUrl, {
